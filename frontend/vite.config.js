bd7aa1ab573ca724415c14fa5a4fd4373a8032e2
Levhita Arias <levhita@gmail.com>
2024-10-17 00:44:50 -0600

    Add SSL and Reverse Proxy (#15)
    * Updated dependencies on backend to avoid vulnerabil
    
    * added reverse proxy
    
    * * added basic ssl
    * exposed vite to the local network
    
    ---------
    
    Co-authored-by: Levhita <levhita@photoshelter.com>
    Co-authored-by: Ameli (Meme) Gavante <80282588+memecgavante@users.noreply.github.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    basicSsl({
+      name: 'yamo'
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
