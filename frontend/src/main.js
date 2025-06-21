a0feeb6f5fda609c060af625968ddd911d099b73
Levhita Arias <levhita@gmail.com>
2025-06-20 21:56:35 -0600

    imported with sass

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import "bootstrap/dist/css/bootstrap.min.css"
+import './styles/bootstrap-custom.scss'
 import "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './style.css'
