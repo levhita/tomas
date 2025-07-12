3ebc2e6f3c941adcda3772ebf450c89c98a96588
Levhita Arias <levhita@gmail.com>
2025-07-12 15:41:19 -0600

    removed unusued seeds

--- api/db/test_schema.sql
+++
@@ -1,7 +0,0 @@
--- Test Database Schema
--- This file sources the main schema.sql and test_seeds.sql
--- Include main schema (drop tables and create schema)
-SOURCE schema.sql;
-
--- Include test data
-SOURCE test_seeds.sql;
